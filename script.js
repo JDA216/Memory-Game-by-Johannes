9a01708ea1878a0abba372bb2d338bd718206216
JDA216 <123894533+JDA216@users.noreply.github.com>
2023-02-07 10:39:20 +0100

    Adding several functions for correct memory pair
    not working yet...

--- script.js
+++ script.js
@@ -8,11 +8,35 @@ let flippedCard = false;
 function flipCard() {
   this.classList.addEventListener("flip")
 
-  if (flippedCard){
+  if (!flippedCard){
     flippedCard = true;
     firstCard = this;
     return;
   }
+
+  secondCard = this;
+  flippedCard = false;
+
+  checkMatch();
 }
+
+function checkMatch(){
+    if (firstCard.data-set === secondCard.data-set){
+        lockCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function lockCards(){
+
+}
+
+function unflipCards(){
+
+}
+
+
 
 cards.forEach(card => card.addEventListener("click", flipCard));
